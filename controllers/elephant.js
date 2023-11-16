9a5077124506483fc2734f1785e85b527c2d28e7
Gude <S566615@nwmissouri.edu>
2023-11-15 18:02:32 -0600

    adding style to the one id in the collection

--- controllers/elephant.js
+++ controllers/elephant.js
@@ -93,4 +93,16 @@ exports.elephant_delete = async function(req, res) {
     res.send(`{"error": Error deleting ${err}}`);
     }
     };
-    
+    // Handle a show one view with id specified by query
+exports.elephant_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await elephant.findById( req.query.id)
+    res.render('elephantdetail',
+    { title: 'elephant Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
--- routes/elephant.js
+++ routes/elephant.js
@@ -3,4 +3,6 @@ const elephant_controlers= require('../controllers/elephant');
 var router = express.Router();
 /* GET elephants */
 router.get('/', elephant_controlers.elephant_view_all_Page );
+/* GET detail elephant page */
+router.get('/detail', elephant_controlers.elephant_view_one_Page);
 module.exports = router;
