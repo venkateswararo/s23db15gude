b2021b719a21dda0a6b77cd1961d150c6e399991
Gude <S566615@nwmissouri.edu>
2023-11-15 19:56:50 -0600

    using the operation Update and commiting

--- controllers/elephant.js
+++ controllers/elephant.js
@@ -121,5 +121,17 @@ exports.elephant_view_one_Page = async function(req, res) {
     }
     };
 
-    
+    // Handle building the view for updating a costume.
+    // query provides the id
+    exports.elephant_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await elephant.findById(req.query.id)
+    res.render('elephantupdate', { title: 'elephant Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
     
--- routes/elephant.js
+++ routes/elephant.js
@@ -7,4 +7,6 @@ router.get('/', elephant_controlers.elephant_view_all_Page );
 router.get('/detail', elephant_controlers.elephant_view_one_Page);
 /* GET create elephant page */
 router.get('/create', elephant_controlers.elephant_create_Page);
+/* GET create update page */
+router.get('/update', elephant_controlers.elephant_update_Page);
 module.exports = router;
